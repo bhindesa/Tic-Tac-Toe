1d923fc937500ba1f2a9174aad153271bb0b4986
Sarb Bhinder <sarb8275@gmail.com>
2022-09-24 22:04:06 -0400

    Added layout to the github

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+
+
+
+//access all the grid cells
+//when page load clear all grid cells
+
+/*----- constants -----*/
+const messageAreaEl = $('#messageArea');
+const gamePlayAreaEl = $('#gamePlayArea');
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+
+function init(){
+
+}
+
+function render(){
+
+}
